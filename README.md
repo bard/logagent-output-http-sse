432921b94dda32b45fb3178fb88e7dda7503fb38
Massimiliano Mirra <bard@hyperstruct.net>
2018-09-11 12:37:53 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,52 @@
+An HTTP server-side events plugin for [logagent](https://github.com/sematext/logagent-js). Can be used for e.g. writing a custom web log live tail UI.
+
+## Installation
+
+On the machine where you're running logagent:
+
+```
+npm install -g logagent-output-http-sse
+```
+
+## Configuration
+
+In logagent's `config.yml` output section, add:
+
+```
+output:
+  http-sse:
+    module: logagent-output-http-sse
+    port: 80
+```
+
+Adjust port as needed. If omitted, it will listen on 3000.
+
+## Usage
+
+Assuming logagent is running on `log.example.com`, the following will stream all log events to the browser console:
+
+```
+<html>
+  <body>
+    <script>
+     const eventSource = new EventSource('http://log.example.com/events')
+
+     eventSource.onmessage = (event) =>
+       console.log(event.data)
+     }
+    </script>
+  </body>
+</html>
+```
+
+To request events from certain sources only, use a "source" query parameter with an  [extglob](https://www.npmjs.com/package/extglob) pattern value.
+
+For example, to only request events from sources beginning with "myapp_":
+
+```
+     const eventSource = new EventSource('http://log.example.com/events?source=myapp_*')
+```
+
+## Security
+
+There is no authorization or encryption, please use a reverse proxy to provide those if running on an untrusted network.
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const {makeRe} = require('extglob')
+const safeStringify = require('fast-safe-stringify')
+const {sse} = require('@toverux/expresse')
+
+function OutputHTTP (config, eventEmitter) {
+  this.config = config
+  this.eventEmitter = eventEmitter
+  this.app = express()
+  this.clients = new Set()
+}
+
+OutputHTTP.prototype.start = function () {
+  this.eventEmitter.on('data.parsed', this.eventHandler.bind(this))
+
+  this.app.set('env', 'production')
+  this.app.get('/ping', (req, res) => res.end('logagent'))
+
+  this.app.get('/events', (req, res, next) => {
+    if (this.config.debug) {
+      console.log('logagent-output-http: client connecting')
+    }
+
+    const client = {
+      req,
+      res,
+      sourceFilter: req.query.source
+        ? makeRe(req.query.source)
+        : null
+    }
+
+    res.set('access-control-allow-origin', '*')
+    res.once('close', () => this.clients.delete(client))
+    res.once('finish', () => this.clients.delete(client))
+    this.clients.add(client)
+    next()
+  }, sse({ serializer: safeStringify }))
+
+  this.server = this.app.listen(this.config.port || 3000, this.config.address || '0.0.0.0', () => {
+    if (this.config.debug) {
+      console.log('logagent-output-http: server started')
+    }
+  })
+}
+
+OutputHTTP.prototype.stop = function (cb) {
+  this.eventEmitter.removeListener('data.parsed', this.eventHandler)
+  this.server.close()
+  cb()
+}
+
+OutputHTTP.prototype.eventHandler = function (data, context) {
+  if (this.config.suppress) {
+    return
+  }
+
+  if (this.config.debug) {
+    console.log('logagent-output-http: sending log data to clients')
+  }
+
+  this.clients.forEach(c => {
+    if (this.config.debug) {
+      console.log('testing ', c)
+      console.log('data', data)
+    }
+    if (!c.sourceFilter || c.sourceFilter.test(data.logSource)) {
+      c.res.sse.event('message', data)
+    }
+  })
+}
+
+module.exports = OutputHTTP
